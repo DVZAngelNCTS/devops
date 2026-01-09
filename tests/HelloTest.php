d1f41f33be782cda45a408dbd8f6706cbee80387
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-09 14:04:56 +0100

    test uni

---
+++ tests/HelloTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Hello;
+
+class HelloTest extends TestCase
+{
+    public function testSayHelloEmpty(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, !', $hello->sayHello(''));
+    }
+
+    public function testSpecialCharacters(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, Élodie!', $hello->sayHello('Élodie'));
+    }
+
+    public function testNumericName(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, 123!', $hello->sayHello('123'));
+    }
+
+    public function testReturnType(): void
+    {
+        $hello = new Hello();
+        $this->assertIsString($hello->sayHello('Bob'));
+    }
+}
